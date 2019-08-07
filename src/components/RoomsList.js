8644143ae8fa6ec46c6b344655aa5b43fd027d81
april <martinezapril56@yahoo.com>
2019-08-07 09:24:44 -0400

    making rooms

---
+++ src/components/RoomsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RoomsFilter from './RoomsFilter';
+import RoomsList from './RoomsList';
+
+
+export default function RoomList() {
+    return (
+        <div>
+           hello to from room roomlist
+           <RoomsFilter/>
+           <RoomsList />
+        </div>
+    )
+}
